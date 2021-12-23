828297f116f019d83e8f4aaf2da44fbed1f91e88
fredtsai <fred412037@gmail.com>
2021-12-23 21:32:28 +0800

    second commit

--- Low_freq_otg_app/app/src/main/java/com/otg_low_freq/WelcomeActivity.java
+++ Low_freq_otg_app/app/src/main/java/com/otg_low_freq/WelcomeActivity.java
@@ -9,12 +9,6 @@ import android.view.Window;
 import android.widget.Button;
 
 
-/**
- * 本APP所有容之智慧財權，均為蝸牛手建館所有，包括文字、照片、圖片、軟體程式碼，皆受中華民國著作權法、商標法、
- * 公平交易法與其他相關法令之保障，請尊重智慧財權並遵守法令規範，請勿自行使用、轉載、修改、重製、發行、公開發表、教學、散佈，
- * 若需此方面的使用或服務，請務必事先與蝸牛手建館人員接洽，並且取得授權。
- */
-
 public class WelcomeActivity extends Activity {
 
     private Button button;
